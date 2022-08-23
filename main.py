f4577d2c3ae8fe11d6925d83fc5e515ee677f857
Clartik <yg.kartik@gmail.com>
2022-08-22 20:52:04 -0700

    Updated Functionality

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from canvasapi import Canvas
+import canvasapi
+import datetime
 
-canvas = Canvas("https://vcccd.instructure.com/", "5499~BXHt8xRg7WotXtqaA3JkJByvZGbXzPF5ortw6fgOMQwOVIKtINAPMTHmrWS1ZD7Z")
+canvas = canvasapi.Canvas("https://vcccd.instructure.com/", "5499~BXHt8xRg7WotXtqaA3JkJByvZGbXzPF5ortw6fgOMQwOVIKtINAPMTHmrWS1ZD7Z")
 
 courses = canvas.get_courses(enrollment_state='active')
 
+def ConvertToDateTime(time: str) -> datetime.datetime:
+    convertedTime = datetime.datetime.strptime(time, '%Y-%m-%dT%H:%M:%SZ')
+    convertedTime = convertedTime - datetime.timedelta(hours=7)
+    convertedTime = convertedTime.strftime('%Y-%m-%d %I:%M:%S %p')
+    return convertedTime
+
 for course in courses:
-    print(course.name)
-    for assignment in course.get_assignments():
-        print(assignment.name)
+    print(f'*** {course.name} ***')
+    for assignment in course.get_assignments(bucket='upcoming', order='due_at'):
+        assignmentDueDate = ConvertToDateTime(assignment.due_at)
+        assignmentUnlockDate = ConvertToDateTime(assignment.unlock_at)
+        currentDate = datetime.datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
+        if currentDate <= assignmentUnlockDate:
