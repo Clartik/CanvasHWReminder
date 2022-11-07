50137c0028ce44f5fabdc5e3ed048cffc8540697
Clartik <96754277+Clartik@users.noreply.github.com>
2022-11-06 20:03:27 -0800

    Did something

--- GUI/main.js
+++ GUI/main.js
@@ -1,17 +1,24 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences:
+    {
+      preload: path.join(__dirname, 'preload.js'),
+    },
   });
 
+  ipcMain.handle('ping', () => 'pong');
   win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
   createWindow();
 
+  // For Mac OS
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
