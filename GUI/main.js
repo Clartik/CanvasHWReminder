b5277fdd6d0cc72f7a4e4b608a8a1e8039d77631
Clartik <yg.kartik@gmail.com>
2022-11-09 22:24:59 -0800

    Started Work on Main Page

--- GUI/main.js
+++ GUI/main.js
@@ -1,24 +1,34 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+const {PythonShell} = require('python-shell');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
-    webPreferences:
-    {
-      preload: path.join(__dirname, 'preload.js'),
-    },
+    height: 600
   });
 
-  ipcMain.handle('ping', () => 'pong');
-  win.loadFile('index.html');
+  var toggler = document.getElementsByClassName("caret");
+  var i;
+
+  for (i = 0; i < toggler.length; i++) {
+    toggler[i].addEventListener("click", function() {
+      this.parentElement.querySelector(".nested").classList.toggle("active");
+      this.classList.toggle("caret-down");
+    });
+  }
+
+  win.loadFile('mainPage.html');
+
+  // PythonShell.run('test.py', null, function(err) {
+  //   if (err) throw err;
+  //   console.log('UH OH');
+  // })
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  // For Mac OS
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
@@ -27,6 +37,8 @@ app.whenReady().then(() => {
 });
 
 app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin')
     {
         app.quit()
