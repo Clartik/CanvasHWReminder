ebd644a07e960479ef76b3eddcf679a5f8715d4c
Clartik <yg.kartik@gmail.com>
2022-11-09 22:24:59 -0800

    Started Work on Main Page

--- GUI/preload.js
+++
@@ -1,9 +0,0 @@
-const {contextBridge, ipcRenderer} = require('electron');
-
-contextBridge.exposeInMainWorld('versions',
-{
-    node: () => process.versions.node,
-    chrome: () => process.versions.chrome.chrome,
-    electron: () => process.versions.electron,
-    ping: () => ipcRenderer.invoke('ping'),
-})
