0dfadd5980212d122e6413bd3505fc2dcd464a02
Clartik <yg.kartik@gmail.com>
2024-07-26 16:17:52 -0700

    Finally Getting Somewhere with Dropdowns

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 const { app, BrowserWindow } = require('electron');
 
-// const {PythonShell} = require('python-shell');
-
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600
   });
 
-  win.loadFile('mainPage.html');
-
-  // PythonShell.run('test.py', null, function(err) {
-  //   if (err) throw err;
-  //   console.log('UH OH');
-  // })
-
+  win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
