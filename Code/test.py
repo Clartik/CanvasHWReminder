800ef50459fa7b42482925e957614f2a59668599
Clartik <yg.kartik@gmail.com>
2024-07-26 15:25:01 -0700

    Reworked Project Structure

--- Code/test.py
+++
@@ -1,7 +0,0 @@
-from winotify import Notification
-
-toast = Notification('Neural Nine Script', 'Message Title',
-                     msg='Hello Worldaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-                     duration='short')
-
-toast.show()
