4e8bed25912331888ade52d90ebf596407633c6d
Clartik <yg.kartik@gmail.com>
2024-08-05 15:34:29 -0700

    Added Dialog to Save in Settings Page. Added Loading Page

--- src/preload.ts
+++ src/preload.ts
@@ -2,7 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 const API = {
     getJSONData: (filename: string) => ipcRenderer.invoke('json:getData', filename),
-    openLink: (url: string) => ipcRenderer.send('openLink', url)
+    openLink: (url: string) => ipcRenderer.send('openLink', url),
+    showMessageDialog: (options: Electron.MessageBoxOptions) => ipcRenderer.invoke('showMessageDialog', options)
 }
 
 contextBridge.exposeInMainWorld("api", API);
--- src/settings.ts
+++ src/settings.ts
@@ -1,16 +1,28 @@
+let backBtnAnchor = document.getElementById('home-link') as HTMLAnchorElement | null;
 let canvasAPIBtn = document.getElementById('canvasAPI-btn') as HTMLButtonElement | null;
 let canvasAPIInput = document.getElementById('canvasAPI-input') as HTMLInputElement | null;
 let timeDropdown = document.getElementById('time-dropdown') as HTMLSelectElement | null;
 let timeFormatDropdown = document.getElementById('time-format-dropdown') as HTMLSelectElement | null;
+let changeableElements = document.getElementsByClassName('changeable');
 
 const DAY_TIME_OPTIONS: Array<string> = [];
 const HOUR_TIME_OPTIONS: Array<string> = [];
 const MINUTE_TIME_OPTIONS: Array<string> = [];
 
 let canvasAPIEditMode = false;
+let hasSettingsChanged = false;
 
 populateTimeOptions();
 populateTimeDropdownWithCorrectFormatOptions();
+
+for (let i = 0; i < changeableElements.length; i++) {
+    const element = changeableElements[i];
+
+    element.addEventListener('change', () => {
+        if (!hasSettingsChanged)
+            hasSettingsChanged = true;
+    });
+};
 
 canvasAPIBtn?.addEventListener('click', (event: MouseEvent) => {
     if (!canvasAPIEditMode) {
@@ -37,16 +49,43 @@ timeFormatDropdown?.addEventListener('change', (event: Event) => {
     populateTimeDropdownWithCorrectFormatOptions();
 });
 
+backBtnAnchor?.addEventListener('click', async (event: MouseEvent) => {
+    event.preventDefault();
+
+    if (hasSettingsChanged) {
+        const options: Electron.MessageBoxOptions = {
+            type: "warning",
+            title: "Save Settings?",
+            message: "Do You Want to Save?",
+            buttons: ['Yes', 'No'],
+            defaultId: 0
+        }
+        
+        const messageResponse: Electron.MessageBoxReturnValue = await window.api.showMessageDialog(options);
+    
+        if (messageResponse.response === 0) {
+            console.log('Save!!!');
+        }
+    }
+
+    // Tells it to load the other page
+    if (backBtnAnchor !== null)
+        window.location.href = backBtnAnchor.href;
+});
+
 function populateTimeOptions() {
-    for (let i = 1; i <= 5; i++) {
+    // Includes 1 to 7
+    for (let i = 1; i <= 7; i++) {
         DAY_TIME_OPTIONS.push('' + i);
     }
 
-    for (let i = 1; i <= 24; i++) {
+    // Includes 1 to 23
+    for (let i = 1; i <= 23; i++) {
         HOUR_TIME_OPTIONS.push('' + i);
     }
 
-    for (let i = 1; i <= 60; i++) {
+    // Includes 1 to 59
+    for (let i = 1; i <= 59; i++) {
         MINUTE_TIME_OPTIONS.push('' + i);
     }
 }
