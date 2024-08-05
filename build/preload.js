09de597e0778ad86025bdf724a864f1cd96cf501
Clartik <yg.kartik@gmail.com>
2024-08-05 15:34:56 -0700

    test

--- build/preload.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const { contextBridge, ipcRenderer } = require('electron');
-const API = {
-    getJSONData: (filename) => ipcRenderer.invoke('json:getData', filename),
-    openLink: (url) => ipcRenderer.send('openLink', url)
-};
-contextBridge.exposeInMainWorld("api", API);
