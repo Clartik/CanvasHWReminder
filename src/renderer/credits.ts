5221a326164b20fd02427b3088bdad35c279d0de
Clartik <yg.kartik@gmail.com>
2024-11-02 23:31:19 -0700

    Removed social media buttons from credits and added correct link for website. Also genericifed setup file name for aws

--- src/renderer/credits.ts
+++ src/renderer/credits.ts
@@ -1,16 +1,16 @@
-const githubLinkBtn = document.getElementById('github-btn')! as HTMLButtonElement;
+// const githubLinkBtn = document.getElementById('github-btn')! as HTMLButtonElement;
 const websiteLinkBtn = document.getElementById('website-btn')! as HTMLButtonElement;
-const youtubeLinkBtn = document.getElementById('youtube-btn')! as HTMLButtonElement;
+// const youtubeLinkBtn = document.getElementById('youtube-btn')! as HTMLButtonElement;
 // const instagramLinkBtn = document.getElementById('instragram-link-btn')! as HTMLButtonElement;
 
-githubLinkBtn.addEventListener('click', () => {
-    window.api.openLink('https://github.com/Clartik/CanvasHWReminder');
-})
+// githubLinkBtn.addEventListener('click', () => {
+//     window.api.openLink('https://github.com/Clartik/CanvasHWReminder');
+// })
 
 websiteLinkBtn.addEventListener('click', () => {
-    window.api.openLink('https://trello.com/b/cdFdMcDf/canvas-hw-reminder');
+    window.api.openLink('https://clartik.github.io/CanvasHWReminder-Website');
 });
 
-youtubeLinkBtn.addEventListener('click', () => {
-    window.api.openLink('https://www.youtube.com/@kartech6079')
-});
+// youtubeLinkBtn.addEventListener('click', () => {
+//     window.api.openLink('https://www.youtube.com/@kartech6079')
+// });
