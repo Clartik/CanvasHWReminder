8a332b749a93060e5725e7e8b14eb664f4a0af8a
Clartik <yg.kartik@gmail.com>
2024-12-06 10:55:36 -0800

    Switched to using actual website for credit links and added click in effect for page buttons

--- src/renderer/credits.ts
+++ src/renderer/credits.ts
@@ -1,11 +1,16 @@
 const websiteLinkBtn = document.getElementById('website-btn')! as HTMLButtonElement;
 
 const termsLinkCredits = document.getElementById('terms-link')! as HTMLSpanElement;
+const privacyLinkCredits = document.getElementById('privacy-link')! as HTMLSpanElement;
 
 websiteLinkBtn.addEventListener('click', () => {
-    window.api.openLink('https://clartik.github.io/CanvasHWReminder-Website');
+    window.api.openLink('https://canvashwreminder.com/');
 });
 
 termsLinkCredits.addEventListener('click', () => {
-    window.api.openLink('https://clartik.github.io/CanvasHWReminder-Website/tos.html');
+    window.api.openLink('https://canvashwreminder.com/tos');
+})
+
+privacyLinkCredits.addEventListener('click', () => {
+    window.api.openLink('https://canvashwreminder.com/privacy');
 })
